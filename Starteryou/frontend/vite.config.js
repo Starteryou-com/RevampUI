b6c0facff00cdc46ac1f1fb0c9b39118e48db2a0
Nikshep A Kulli <48430792+nikshepkulli@users.noreply.github.com>
2024-11-01 23:20:32 -0400

    testing --development for copmose

--- Starteryou/frontend/vite.config.js
+++ Starteryou/frontend/vite.config.js
@@ -3,24 +3,15 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
-  // Set NODE_ENV based on the mode
-  if (mode === "production") {
-    process.env.NODE_ENV = "production";
-  } else {
-    process.env.NODE_ENV = "development";
-  }
-
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-        "@hooks": path.resolve(__dirname, "./src/hooks"),
-        "@components": path.resolve(__dirname, "./src/components"),
-        "@utils": path.resolve(__dirname, "./src/utils"),
-        "@config": path.resolve(__dirname, "./src/config"),
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@config": path.resolve(__dirname, "./src/config"),
     },
-  };
+  },
 });
